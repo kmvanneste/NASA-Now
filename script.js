696f26ed0e96fa1b5fcf574efbce5ac544c9d678
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-04 13:48:20 -0400

    created and cleaned up youtube js file

--- script.js
+++ script.js
@@ -1,57 +1,10 @@
-
-var nasakey = "fdlQhb62Szn7dtpYyag7qcPGVprhsOxQDYoXgeQ9"
-
-
-function nasaCall(searchTerm) {
-    let queryURL = "https://images-api.nasa.gov/search?q=" + searchTerm + "&media_type=image"
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        // $("#nasa-dump").text(JSON.stringify(response));
-        console.log(response);
-        createCarousel(getNASALinks(response.collection.items.slice(0, 10)))
-        // $(".carousel-image").each(function (index) {
-        //     imgLink = response.collection.items[index]
-        //     $(this).attr("src", response.collection.items[index].links[0].href);
-        // })
-    })
-}
-
-function getNASALinks(list) {
-    let arr = [];
-    list.forEach(element => {
-        arr.push(element.links[0].href);
-    });
-    return arr
-}
-
-
-function createCarousel(list) {
-    let carouselContainer = $("#carousel-content");
-    carouselContainer.empty();
-    list.forEach(element => {
-        console.log(element)
-        let carouselItem = $("<div>");
-        carouselItem.addClass("carousel-item");
-        let carouselImage = $("<img>");
-        carouselImage.attr("src", element)
-        carouselImage.addClass("d-block carousel-image mx-auto");
-        carouselImage.appendTo(carouselItem);
-        carouselItem.appendTo(carouselContainer);
-
-    });
-    $("#carousel-content div:first").addClass("active");
-}
-
-
 // YouTube API CALL 
     var key = "AIzaSyBihort7mkhzu-EAB_W3I-b1s6RIXYGgl8";
     var queryURL = "https://www.googleapis.com/youtube/v3/search?maxResults=1&q=NASA&key=" + key;
     var backgroundURL;
 
 
-function getAPOD() {
+function getYouTubeVideo() {
     var queryURL = "https://api.nasa.gov/planetary/apod?api_key=" + nasakey;
    
     $.ajax({
@@ -84,44 +37,6 @@ function getAPOD() {
     //               function(err) { console.error("Execute error", err); });
     //   }
 
-
-
-        pushAPOD(response);
-
-}
-
-function pushAPOD(response) {
-    // console.log(response);
-    backgroundURL = response.hdurl;
-    let apodImg = $("<img>").attr("src", backgroundURL);
-    apodImg.css("max-width", "100%")
-    let textp = $("<p>").text(response.explanation)
-    // textp.css("background-color", )
-    $("#apod").append(apodImg, textp);
-    // $("body").css("background-image", "url(" + backgroundURL + ")");
-}
-
-function getMars() {
-    let queryURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity//latest_photos?api_key=" + nasakey
-    // + +"&api_key=" + nasakey
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(response => {
-        console.log(response);
-        // postMars(response);
-    })
-}
-
-function getEPIC() {
-    let queryURL = "https://api.nasa.gov/EPIC/api/enhanced/images?api_key=" + nasakey
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(response => {
-        // console.log(response);
-        // postEPIC(response);
-    })
 }
 
 $(document).ready(x => {
@@ -129,23 +44,3 @@ $(document).ready(x => {
     getEPIC();
     getMars();
 });
-
-
-// nasaCall("Apollo 11");
-
-// function createCarousel(list) {
-//     let carouselContainer = $("#carousel-content");
-//     carouselContainer.empty();
-//     list.forEach(element => {
-//         console.log(element)
-//         let carouselItem = $("<div>");
-//         carouselItem.addClass("carousel-item");
-//         let carouselImage = $("<img>");
-//         carouselImage.attr("src", element)
-//         carouselImage.addClass("d-block carousel-image mx-auto");
-//         carouselImage.appendTo(carouselItem);
-//         carouselItem.appendTo(carouselContainer);
-        
-//     });
-//     $("#carousel-content div:first").addClass("active");
-// }
