7ff7bd044d558bddf5c1332cd181b41becfb831c
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-04 19:33:35 -0400

    working on embeded player

--- youtube.js
+++ youtube.js
@@ -1,18 +1,46 @@
-// YouTube API CALL 
-var key = "AIzaSyBihort7mkhzu-EAB_W3I-b1s6RIXYGgl8";
-var queryURL = "https://www.googleapis.com/youtube/v3/search?maxResults=1&q=NASA&key=" + key;
+$(document).ready((x) => {
+  // YouTube API CALL
+  var key = "AIzaSyBihort7mkhzu-EAB_W3I-b1s6RIXYGgl8";
+  var queryURL =
+    "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=NASA&key=" +
+    key;
 
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  })
+  .then(function (response) {
+    console.log(queryURL);
+    console.log(response);
+  });
 
-function getYouTubeVideo() {
+  var tag = document.createElement("script");
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    
-    .then(function (response) {
-        console.log(queryURL);
-        console.log(response);
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName("script")[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player("player", {
+      height: "390",
+      width: "640",
+      videoId: "M7lc1UVf-VE",
+      playerVars: { 'rel': 0}
     });
-}
+  }
 
+  // 4. The API will call this function when the video player is ready.
+  function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+
+  // 5. The API calls this function when the player's state changes.
+  //    The function indicates that when playing a video (state=1),
+  //    the player should play for six seconds and then stop.
+  function stopVideo() {
+    player.stopVideo();
+  }
+});
