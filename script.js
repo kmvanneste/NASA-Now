1f03e618b0efca7a6a52a96cf27297eeadff928f
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-04 13:19:35 -0400

    adding correct youtube ajax call

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+
+var nasakey = "fdlQhb62Szn7dtpYyag7qcPGVprhsOxQDYoXgeQ9"
+
+
 function nasaCall(searchTerm) {
-
-    let key = "fdlQhb62Szn7dtpYyag7qcPGVprhsOxQDYoXgeQ9"
-
     let queryURL = "https://images-api.nasa.gov/search?q=" + searchTerm + "&media_type=image"
     $.ajax({
         url: queryURL,
@@ -9,7 +10,7 @@ function nasaCall(searchTerm) {
     }).then(function (response) {
         // $("#nasa-dump").text(JSON.stringify(response));
         console.log(response);
-        createCarousel(getNASALinks(response.collection.items.slice(0,10)))
+        createCarousel(getNASALinks(response.collection.items.slice(0, 10)))
         // $(".carousel-image").each(function (index) {
         //     imgLink = response.collection.items[index]
         //     $(this).attr("src", response.collection.items[index].links[0].href);
@@ -17,20 +18,13 @@ function nasaCall(searchTerm) {
     })
 }
 
-$("#search-button").on("click", function (event) {
-    event.preventDefault();
-    search = $("#search-input").val()
-    nasaCall(search);
-})
-
-function getNASALinks(list){
+function getNASALinks(list) {
     let arr = [];
     list.forEach(element => {
         arr.push(element.links[0].href);
     });
     return arr
 }
-
 
 
 function createCarousel(list) {
@@ -45,20 +39,26 @@ function createCarousel(list) {
         carouselImage.addClass("d-block carousel-image mx-auto");
         carouselImage.appendTo(carouselItem);
         carouselItem.appendTo(carouselContainer);
-        
+
     });
     $("#carousel-content div:first").addClass("active");
 }
 
+<<<<<<< HEAD
 // YouTube API CALL 
     var key = "AIzaSyBihort7mkhzu-EAB_W3I-b1s6RIXYGgl8";
     var queryURL = "https://www.googleapis.com/youtube/v3/search?maxResults=1&q=NASA&key=" + key;
     var backgroundURL;
 
+=======
+function getAPOD() {
+    var queryURL = "https://api.nasa.gov/planetary/apod?api_key=" + nasakey;
+>>>>>>> 1391f34088767e7ccad5c50037b6bd60f500ae02
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(response => {
+<<<<<<< HEAD
         console.log(response);
     });
 
@@ -85,6 +85,67 @@ function createCarousel(list) {
     //   }
 
 
+=======
+        pushAPOD(response);
+    });
+}
 
+function pushAPOD(response) {
+    // console.log(response);
+    backgroundURL = response.hdurl;
+    let apodImg = $("<img>").attr("src", backgroundURL);
+    apodImg.css("max-width", "100%")
+    let textp = $("<p>").text(response.explanation)
+    // textp.css("background-color", )
+    $("#apod").append(apodImg, textp);
+    // $("body").css("background-image", "url(" + backgroundURL + ")");
+}
+
+function getMars() {
+    let queryURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity//latest_photos?api_key=" + nasakey
+    // + +"&api_key=" + nasakey
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(response => {
+        console.log(response);
+        // postMars(response);
+    })
+}
+
+function getEPIC() {
+    let queryURL = "https://api.nasa.gov/EPIC/api/enhanced/images?api_key=" + nasakey
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(response => {
+        // console.log(response);
+        // postEPIC(response);
+    })
+}
+
+$(document).ready(x => {
+    getAPOD();
+    getEPIC();
+    getMars();
+});
+>>>>>>> 1391f34088767e7ccad5c50037b6bd60f500ae02
 
 // nasaCall("Apollo 11");
+
+// function createCarousel(list) {
+//     let carouselContainer = $("#carousel-content");
+//     carouselContainer.empty();
+//     list.forEach(element => {
+//         console.log(element)
+//         let carouselItem = $("<div>");
+//         carouselItem.addClass("carousel-item");
+//         let carouselImage = $("<img>");
+//         carouselImage.attr("src", element)
+//         carouselImage.addClass("d-block carousel-image mx-auto");
+//         carouselImage.appendTo(carouselItem);
+//         carouselItem.appendTo(carouselContainer);
+        
+//     });
+//     $("#carousel-content div:first").addClass("active");
+// }
