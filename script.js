172c2960959e7e92db9066f8658a58317fc28538
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-04 13:15:35 -0400

    updating youtube api key

--- script.js
+++ script.js
@@ -51,50 +51,38 @@ function createCarousel(list) {
 }
 
 // YouTube API CALL 
-    var key = "fdlQhb62Szn7dtpYyag7qcPGVprhsOxQDYoXgeQ9";
-    var queryURL = "https://api.nasa.gov/planetary/apod?api_key=" + key;
+    var key = "AIzaSyBihort7mkhzu-EAB_W3I-b1s6RIXYGgl8";
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?maxResults=1&q=NASA&key=" + key;
     var backgroundURL;
 
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(responseY => {
-        console.log(responseY);
-        backgroundURL = responseY.hdurl;
-        textp = $("<p>").text(responseY.explanation)
-        textp.css("background-color", )
-        $("main").append(textp)
-
-        // let img = $("<img>");
-        // img.attr("src", backgroundURL);
-        // img.appendTo($("main"));
-        $("body").css("background-image", "url(" + backgroundURL + ")");
+    }).then(response => {
+        console.log(response);
     });
 
-      function loadClient() {
-        gapi.client.setApiKey("AIzaSyBihort7mkhzu-EAB_W3I-b1s6RIXYGgl8");
-        return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-            .then(function() { console.log("GAPI client loaded for API"); },
-                  function(err) { console.error("Error loading GAPI client for API", err); });
-      }
-      // Make sure the client is loaded and sign-in is complete before calling this method.
-      function execute() {
-        return gapi.client.youtube.search.list({
-          "part": [
-            "snippet"
-          ],
-          "maxResults": 3,
-          "q": "NASA"
-        })
-            .then(function(response) {
-                    // Handle the results here (response.result has the parsed body).
-                    console.log("Response", response);
-                  },
-                  function(err) { console.error("Execute error", err); });
-      }
-
-      loadClient();
-      execute();    
+    //   function loadClient() {
+    //     gapi.client.setApiKey("AIzaSyBihort7mkhzu-EAB_W3I-b1s6RIXYGgl8");
+    //     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+    //         .then(function() { console.log("GAPI client loaded for API"); },
+    //               function(err) { console.error("Error loading GAPI client for API", err); });
+    //   }
+    //   // Make sure the client is loaded and sign-in is complete before calling this method.
+    //   function execute() {
+    //     return gapi.client.youtube.search.list({
+    //       "part": [
+    //         "snippet"
+    //       ],
+    //       "maxResults": 3,
+    //       "q": "NASA"
+    //     })
+    //         .then(function(response) {
+    //                 // Handle the results here (response.result has the parsed body).
+    //                 console.log("Response", response);
+    //               },
+    //               function(err) { console.error("Execute error", err); });
+    //   }
 
 
 
