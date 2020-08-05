85ec6a9dce7398edc4136fd565dc668d969d6ee6
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-05 14:11:45 -0400

    commiting to pull current files

--- youtube.js
+++ youtube.js
@@ -39,26 +39,37 @@ $(document).ready((x) => {
   }
 
   // The API will call this function when the video player is ready.
-  function onPlayerReady(event) {
-    event.target.playVideo();
-  }
+  // function onPlayerReady(event) {
+  //   event.target.playVideo();
+  // }
 
-  function stopVideo() {
-    player.stopVideo();
-  }
+  // function stopVideo() {
+  //   player.stopVideo();
+  // }
 
   // Modal function to get and play youtube video from data tag
   function autoPlayYouTubeModal() {
+
+
     var trigger = $("body").find('[data-toggle="modal"]');
+
+
     trigger.on("click", function() {
-      var theModal = $(this).data("target"),
-      videoSRC = $(this).attr("data-theVideo"),
-      videoSRCauto = videoSRC +"?autoplay=1";
+
+      var theModal = $(this).data("target");
+      var videoSRC = $(this).attr("data-theVideo"); 
+      var videoSRCauto = videoSRC +"?autoplay=1";
+
     $(theModal + 'iframe').attr('src', videoSRCauto);
+
     $(theModal + 'button.close').on("click", function() {
+
       $(theModal + 'iframe').attr('src', videoSRC);
+
     });
     });
+
+
   }
   
 });
