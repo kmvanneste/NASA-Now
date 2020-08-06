86ceb173ffeacff5d2a5da62f87e26ef22ac53a3
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-06 13:47:46 -0400

    removed auto play from video

--- assets/scripts/youtube.js
+++ assets/scripts/youtube.js
@@ -25,6 +25,8 @@ $(document).ready((x) => {
 
   });
 
+  // console.log(videoId);
+  
     // YouTube Provided iFrame
       // This code loads the IFrame Player API code asynchronously.
       var tag = document.createElement('script');
@@ -40,28 +42,12 @@ $(document).ready((x) => {
           height: '390',
           width: '640',
           videoId: 'R_mO5uu853I',
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
         });
       }
 
       // The API will call this function when the video player is ready.
       function onPlayerReady(event) {
         event.target.playVideo();
-      }
-
-      // The API calls this function when the player's state changes. The function indicates that when playing a video (state=1), the player should play for six seconds and then stop.
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
-      function stopVideo() {
-        player.stopVideo();
       }
 
       function youTubeModal() {
