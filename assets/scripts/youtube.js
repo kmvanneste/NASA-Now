8a668f6219e394d426847052ef5e4c95d4f2ba23
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-06 11:55:35 -0400

    added 3rd API key for youtube, working on ajax call

--- assets/scripts/youtube.js
+++ assets/scripts/youtube.js
@@ -1,8 +1,9 @@
 $(document).ready((x) => {
   // YouTube API CALL
-  var key = "AIzaSyDSRviXZqJhq-NfohY5q1hhxdjdbnqnPc4";
+  var key = "AIzaSyB9IppNXuEhH6SDwKTqGX2IvXvnTN-u_Jo";
+  // "AIzaSyDSRviXZqJhq-NfohY5q1hhxdjdbnqnPc4";
   // "AIzaSyBihort7mkhzu-EAB_W3I-b1s6RIXYGgl8";
-  // AIzaSyDSRviXZqJhq-NfohY5q1hhxdjdbnqnPc4
+
   var queryURL =
     "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&type=video&q=NASA&key=" +
     key;
@@ -23,15 +24,14 @@ $(document).ready((x) => {
   });
 
     // YouTube Provided iFrame
-      // 2. This code loads the IFrame Player API code asynchronously.
+      // This code loads the IFrame Player API code asynchronously.
       var tag = document.createElement('script');
 
       tag.src = "https://www.youtube.com/iframe_api";
       var firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
+      // This function creates an <iframe> (and YouTube player) after the API code downloads.
       var player;
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
@@ -45,14 +45,12 @@ $(document).ready((x) => {
         });
       }
 
-      // 4. The API will call this function when the video player is ready.
+      // The API will call this function when the video player is ready.
       function onPlayerReady(event) {
         event.target.playVideo();
       }
 
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
+      // The API calls this function when the player's state changes. The function indicates that when playing a video (state=1), the player should play for six seconds and then stop.
       var done = false;
       function onPlayerStateChange(event) {
         if (event.data == YT.PlayerState.PLAYING && !done) {
